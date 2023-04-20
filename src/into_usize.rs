f16dc1422e4dbdadc005c886b308ef2235e83bea
Andrew N. Smith <asmith@owlshares.com>
2023-04-20 16:04:40 -0400

    impl IntoUsize for OrderedFloats

--- src/into_usize.rs
+++ src/into_usize.rs
@@ -1,3 +1,6 @@
+use ordered_float::OrderedFloat;
+use num_traits::AsPrimitive;
+
 macro_rules! impl_into_usize_for_numeric_types {
     ($($t:ty),*) => {
         $(
@@ -18,4 +21,16 @@ impl_into_usize_for_numeric_types! {
     i8, i16, i32, i64, i128,
     u8, u16, u32, u64, u128,
     f32, f64
+}
+
+impl IntoUsize for OrderedFloat<f64> {
+    fn into_usize(&self) -> usize {
+        self.as_()
+    }
+}
+
+impl IntoUsize for OrderedFloat<f32> {
+    fn into_usize(&self) -> usize {
+        self.as_()
+    }
 }
